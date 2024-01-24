a62b9f59e62945956b83bd79c37ef16ac2120f88
Edwin Hernandez <edwinhern.16@gmail.com>
2024-01-24 00:54:56 -0600

    feat: Create openAPI router and add swagger.json route

---
+++ src/api-docs/openAPIRouter.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response, Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import { generateOpenAPIDocument } from './openAPIDocumentGenerator';
+
+export const openAPIRouter: Router = (() => {
+  const router = express.Router();
+  const openAPIDocument = generateOpenAPIDocument();
+
+  router.get('/swagger.json', (_req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(openAPIDocument);
+  });
+
+  router.use('/', swaggerUi.serve, swaggerUi.setup(openAPIDocument));
+
+  return router;
+})();
