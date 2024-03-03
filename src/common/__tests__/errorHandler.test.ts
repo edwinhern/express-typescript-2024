39190d9b95f475734bf98d11ad9e056912b33afc
Edwin Hernandez <Edwinhern.16@gmail.com>
2024-03-03 15:26:02 -0600

    Test/middleware test files (#69)

--- src/common/__tests__/errorHandler.test.ts
+++ src/common/__tests__/errorHandler.test.ts
@@ -1,27 +1,45 @@
-import express from 'express';
+import express, { Express } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import request from 'supertest';
 
 import errorHandler from '@/common/middleware/errorHandler';
 
 describe('Error Handler Middleware', () => {
-  const app = express();
+  let app: Express;
 
-  // Setup a route that throws an error
-  app.get('/error', () => {
-    throw new Error('Test error');
+  beforeAll(() => {
+    app = express();
+
+    app.get('/error', () => {
+      throw new Error('Test error');
+    });
+    app.get('/next-error', (_req, _res, next) => {
+      const error = new Error('Error passed to next()');
+      next(error);
+    });
+
+    app.use(errorHandler());
+    app.use('*', (req, res) => res.status(StatusCodes.NOT_FOUND).send('Not Found'));
   });
 
-  // Use your error handler middleware
-  app.use(errorHandler());
-
-  it('should return 404 for unknown routes', async () => {
-    const response = await request(app).get('/unknown');
-    expect(response.status).toBe(StatusCodes.NOT_FOUND);
+  describe('Handling unknown routes', () => {
+    it('returns 404 for unknown routes', async () => {
+      const response = await request(app).get('/this-route-does-not-exist');
+      expect(response.status).toBe(StatusCodes.NOT_FOUND);
+    });
   });
 
-  it('should handle thrown errors with 500 status code', async () => {
-    const response = await request(app).get('/error');
-    expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
+  describe('Handling thrown errors', () => {
+    it('handles thrown errors with a 500 status code', async () => {
+      const response = await request(app).get('/error');
+      expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
+    });
+  });
+
+  describe('Handling errors passed to next()', () => {
+    it('handles errors passed to next() with a 500 status code', async () => {
+      const response = await request(app).get('/next-error');
+      expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
+    });
   });
 });
