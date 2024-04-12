e216413221e00ab6ddd2e1f3c2f5a187b34c9306
Edwin Hernandez <edwinhern.16@gmail.com>
2024-04-12 14:38:38 -0500

    refactor: :recycle: requestLogger middleware to use error instead of err in requestLogger.ts

--- src/common/middleware/rateLimiter.ts
+++ src/common/middleware/rateLimiter.ts
@@ -2,24 +2,14 @@ import { Request } from 'express';
 import { rateLimit } from 'express-rate-limit';
 
 import { env } from '@/common/utils/envConfig';
-import { logger } from '@/server';
 
 const rateLimiter = rateLimit({
   legacyHeaders: true,
-  limit: env.COMMON_RATE_LIMIT_MAX_REQUESTS ?? 20,
+  limit: env.COMMON_RATE_LIMIT_MAX_REQUESTS,
   message: 'Too many requests, please try again later.',
   standardHeaders: true,
-  windowMs: 15 * 60 * (env.COMMON_RATE_LIMIT_WINDOW_MS ?? 1000),
-  keyGenerator,
+  windowMs: 15 * 60 * env.COMMON_RATE_LIMIT_WINDOW_MS,
+  keyGenerator: (req: Request) => req.ip as string,
 });
-
-function keyGenerator(request: Request): string {
-  if (!request.ip) {
-    logger.warn('Warning: request.ip is missing!');
-    return request.socket.remoteAddress as string;
-  }
-
-  return request.ip.replace(/:\d+[^:]*$/, '');
-}
 
 export default rateLimiter;
--- vite.config.mts
+++ vite.config.mts
@@ -2,6 +2,12 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: { globals: true },
+  test: {
+    coverage: {
+      exclude: ['**/node_modules/**', '**/commitlint.config.ts', '**/release.config.cjs', '**/index.ts'],
+    },
+    globals: true,
+    restoreMocks: true,
+  },
   plugins: [tsconfigPaths()],
 });
