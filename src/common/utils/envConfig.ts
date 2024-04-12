790ae5d0ffd4d4f762f9e30c2da4b4d65a510f82
Edwin Hernandez <Edwinhern.16@gmail.com>
2024-04-12 15:19:13 -0500

    refactor: :recycle: Refactor build workflow and requestLogger middleware (#107)

--- src/common/utils/envConfig.ts
+++ src/common/utils/envConfig.ts
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
-import { cleanEnv, host, num, port, str } from 'envalid';
+import { cleanEnv, host, num, port, str, testOnly } from 'envalid';
 
 dotenv.config();
 
 export const env = cleanEnv(process.env, {
-  NODE_ENV: str({ choices: ['development', 'test', 'production'] }),
-  HOST: host(),
-  PORT: port(),
-  CORS_ORIGIN: str(),
-  COMMON_RATE_LIMIT_MAX_REQUESTS: num(),
-  COMMON_RATE_LIMIT_WINDOW_MS: num(),
+  NODE_ENV: str({ devDefault: testOnly('test'), choices: ['development', 'production', 'test'] }),
+  HOST: host({ devDefault: testOnly('localhost') }),
+  PORT: port({ devDefault: testOnly(3000) }),
+  CORS_ORIGIN: str({ devDefault: testOnly('http://localhost:3000') }),
+  COMMON_RATE_LIMIT_MAX_REQUESTS: num({ devDefault: testOnly(1000) }),
+  COMMON_RATE_LIMIT_WINDOW_MS: num({ devDefault: testOnly(1000) }),
 });
