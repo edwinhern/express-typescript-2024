7aca74926cb67a652a647b0a7b1f03b26da90eb3
Edwin Hernandez <edwinhern.16@gmail.com>
2024-07-17 10:58:35 -0500

    refactor: :recycle: Simplify error handling in validateRequest function

--- src/common/utils/httpHandlers.ts
+++ src/common/utils/httpHandlers.ts
@@ -13,13 +13,9 @@ export const validateRequest = (schema: ZodSchema) => (req: Request, res: Respon
     schema.parse({ body: req.body, query: req.query, params: req.params });
     next();
   } catch (err) {
-    if (err as ZodError) {
-      const errorMessage = `Invalid input: ${(err as ZodError).errors.map((e) => e.message).join(", ")}`;
-      const statusCode = StatusCodes.BAD_REQUEST;
-      const serviceResponse = ServiceResponse.failure(errorMessage, null, statusCode);
-      return handleServiceResponse(serviceResponse, res);
-    }
-    const serviceResponse = ServiceResponse.failure("Invalid input", null, StatusCodes.BAD_REQUEST);
+    const errorMessage = `Invalid input: ${(err as ZodError).errors.map((e) => e.message).join(", ")}`;
+    const statusCode = StatusCodes.BAD_REQUEST;
+    const serviceResponse = ServiceResponse.failure(errorMessage, null, statusCode);
     return handleServiceResponse(serviceResponse, res);
   }
 };
